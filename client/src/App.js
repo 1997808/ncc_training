4d962bb8d9c09b27ffd669326cdb8d889951fd0a
1997808 <khanhhuyle22@gmail.com>
2021-07-20 10:35:35 +0700

    setup fake authen fe

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,46 @@
-function App() {
-  return (
-    <div className="App">
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { ProtectedPage } from "./pages/backend/protectedPage";
+import { PublicPage } from "./pages/publicPage"
+import { PrivateRoute } from "./utils/privateRoute";
+import { LoginPage } from "./pages/login";
+import { AuthButton } from "./components/authButton";
+import { ProvideAuth } from "./utils/provideAuth"
 
-    </div>
+export default function App() {
+  return (
+    <ProvideAuth>
+      <Router>
+        <div>
+          <AuthButton />
+
+          <ul>
+            <li>
+              <Link to="/public">Public Page</Link>
+            </li>
+            <li>
+              <Link to="/protected">Protected Page</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/public">
+              <PublicPage />
+            </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+            <PrivateRoute path="/protected">
+              <ProtectedPage />
+            </PrivateRoute>
+          </Switch>
+        </div>
+      </Router>
+    </ProvideAuth>
   );
 }
-
-export default App;
